0a2a9046bfe4ca3e215b7777610c4528fe9ab88a
Samuel Rinnetmäki <samuel@iki.fi>
2020-11-29 15:18:36 +0100

    converted to UTF-8

--- include/Coicop.php
+++ include/Coicop.php
@@ -6,18 +6,18 @@ require_once('CoicopSub.php');
 class Coicop {
 
 
-  function Coicop() {
+  function __construct() {
     $this->cats = array();
 
     $c01 = new CoicopCat('01', '#FF0000',
                        'Elintarvikkeet ja alkoholittomat juomat',
 		       'Food and non-alcoholic beverages');
     $c01->addSub('01.1', 'Elintarvikkeet', 'Food');
-    $c01->addHelp('01.1.1', 'Viljatuotteet ja leipä', '');
+    $c01->addHelp('01.1.1', 'Viljatuotteet ja leipÃ¤', '');
     $c01->addHelp('01.1.2', 'Liha', '');
     $c01->addHelp('01.1.3', 'Kala', '');
     $c01->addHelp('01.1.4', 'Maitotuotteet, juusto ja kananmunat', '');
-    $c01->addHelp('01.1.5', 'Öjlyt ja rasvat', '');
+    $c01->addHelp('01.1.5', 'Ã–jlyt ja rasvat', '');
     $c01->addHelp('01.1.6', '', '');
     $c01->addHelp('01.1.7', '', '');
     $c01->addHelp('01.1.8', '', '');
@@ -44,22 +44,22 @@ class Coicop {
     $c03->addHelp('03.1.3', 'Asusteet ja pukineet', 'Clothing 3');
     $c03->addHelp('03.1.4', 'Vaatteiden pesu, korjaus ja vuokraus', 'Clothing 4');
     $c03->addSub('03.2', 'Jalkineet', 'Footwear');
-    $c03->addHelp('03.2.1', 'Kengät ja muut jalkineet', 'Footwear 1');
+    $c03->addHelp('03.2.1', 'KengÃ¤t ja muut jalkineet', 'Footwear 1');
     $c03->addHelp('03.2.2', 'Jalkineiden korjaus ja vuokraus', 'Footwear 2');
     $this->cats[] = &$c03;
 
     $c04 = new CoicopCat('04', '#FFCC33',
                        # 'Asuminen ja energia',
-                       'Asuminen, vesi ja sähkö',
+                       'Asuminen, vesi ja sÃ¤hkÃ¶',
 		       'Housing, water, electricity, gas and other fuels');
     # $c04->addSub('04.1', 'Todelliset asumisvuokrat', 'Actual rentals for housing');
     $c04->addSub('04.1', 'Asuntojen vuokrat', 'Actual rentals for housing');
     # $c04->addSub('04.2', 'Laskennalliset asumisvuokrat', 'Imputed rentals for housing');
     $c04->addSub('04.2', 'Omistusasuminen', 'Imputed rentals for housing');
     $c04->addSub('04.3', 'Asunnon huolto ja korjaus', 'Maintenance and repair of the dwelling');
-    # $c04->addSub('04.4', 'Muut asumiseen liittyvät palvelut', 'Water supply and miscellaneous services relating to the dwelling');
-    $c04->addSub('04.4', 'Vesi ja muut asumiseen liittyvät palvelut', 'Water supply and miscellaneous services relating to the dwelling');
-    $c04->addSub('04.5', 'Sähkö, kaasu ja muut polttoaineet', 'Electricity, gas and other fuels');
+    # $c04->addSub('04.4', 'Muut asumiseen liittyvÃ¤t palvelut', 'Water supply and miscellaneous services relating to the dwelling');
+    $c04->addSub('04.4', 'Vesi ja muut asumiseen liittyvÃ¤t palvelut', 'Water supply and miscellaneous services relating to the dwelling');
+    $c04->addSub('04.5', 'SÃ¤hkÃ¶, kaasu ja muut polttoaineet', 'Electricity, gas and other fuels');
     $this->cats[] = &$c04;
 
     $c05 = new CoicopCat('05', '#33CC33', 
@@ -67,21 +67,21 @@ class Coicop {
                        'Kalusteet, kotitalouskoneet ja yleinen kodinhoito',
 		       'Furnishings, household equipment and routine household maintenance');
     # $c05->addSub('05.1', 'Huonekalut, matot ja sisustus', 'Furniture and furnishings, carpets and other floor coverings');
-    $c05->addSub('05.1', 'Huonekalut ja kalusteet, matot ja muut lattianpäällysteet', 'Furniture and furnishings, carpets and other floor coverings');
+    $c05->addSub('05.1', 'Huonekalut ja kalusteet, matot ja muut lattianpÃ¤Ã¤llysteet', 'Furniture and furnishings, carpets and other floor coverings');
     $c05->addSub('05.2', 'Kodintekstiilit', 'Household textiles');
     $c05->addSub('05.3', 'Kodinkoneet', 'Household appliances');
     # $c05->addSub('05.4', 'Lasitavarat, astiat ja taloustavarat', 'Glassware, tableware and household utensils');
     $c05->addSub('05.4', 'Lasitavarat, astiat ja kotitaloustarvikkeet', 'Glassware, tableware and household utensils');
-    $c05->addSub('05.5', 'Kodin ja puutarhan työkalut ja laitteet', 'Tools and equipment for house and garden');
+    $c05->addSub('05.5', 'Kodin ja puutarhan tyÃ¶kalut ja laitteet', 'Tools and equipment for house and garden');
     # $c05->addSub('05.6', 'Kodinhoitotarvikkeet ja -palvelut', 'Goods and services for routine household maintenance');
-    $c05->addSub('05.6', 'Taloudenhoitoon liittyvät tavarat ja palvelut', 'Goods and services for routine household maintenance');
+    $c05->addSub('05.6', 'Taloudenhoitoon liittyvÃ¤t tavarat ja palvelut', 'Goods and services for routine household maintenance');
     $this->cats[] = &$c05;
 
     $c06 = new CoicopCat('06', '#009900', 
                        'Terveys',
 		       'Health');
-    # $c06->addSub('06.1', 'Lääkevalmisteet, hoitolaitteet ja -tarvikkeet', 'Medical products, appliances and equipment');
-    $c06->addSub('06.1', 'Lääkevalmisteet, hoitolaitteet ja -välineet', 'Medical products, appliances and equipment');
+    # $c06->addSub('06.1', 'LÃ¤Ã¤kevalmisteet, hoitolaitteet ja -tarvikkeet', 'Medical products, appliances and equipment');
+    $c06->addSub('06.1', 'LÃ¤Ã¤kevalmisteet, hoitolaitteet ja -vÃ¤lineet', 'Medical products, appliances and equipment');
     $c06->addSub('06.2', 'Avohoitopalvelut', 'Outpatient services');
     $c06->addSub('06.3', 'Sairaalapalvelut', 'Hospital services');
     $this->cats[] = &$c06;
@@ -90,14 +90,14 @@ class Coicop {
                        'Liikenne',
 		       'Transport');
     $c07->addSub('07.1', 'Ajoneuvojen hankinta', 'Purchase of vehicles');
-    $c07->addSub('07.2', 'Yksityisajoneuvojen käyttö', 'Operation of personal transport equipment');
+    $c07->addSub('07.2', 'Yksityisajoneuvojen kÃ¤yttÃ¶', 'Operation of personal transport equipment');
     # $c07->addSub('07.3', 'Liikennepalvelut', 'Transport services');
     $c07->addSub('07.3', 'Julkinen liikenne', 'Transport services');
     $this->cats[] = &$c07;
 
     $c08 = new CoicopCat('08', '#0033CC',
                        # 'Tietoliikenne',
-                       'Viestintä',
+                       'ViestintÃ¤',
 		       'Communication');
     $c08->addSub('08.1', 'Postipalvelut', 'Postal services');
     $c08->addSub('08.2', 'Puhelin- ja telekopiolaitteet', 'Telephone and telefax equipment');
@@ -108,16 +108,16 @@ class Coicop {
                        'Kulttuuri ja vapaa-aika',
 		       'Recreation and culture');
     # $c09->addSub('09.1', 'Audiovisuaaliset laitteet ja tietokoneet', 'Audio-visual, photographic and information processing equipment');
-    $c09->addSub('09.1', 'Audiovisuaaliset sekä valokuvaus- ja tietojenkäsittelylaitteet', 'Audio-visual, photographic and information processing equipment');
-    $c09->addHelp('09.1.1', 'Äänen ja kuvan vastaanottoon, tallentamiseen ja toistoon käytettävät laitteet', 'Audio-visual, photographic and information processing equipment 1');
-    $c09->addHelp('09.1.2', 'Valokuvaus- ja elokuvalaitteet sekä optiset laitteet', 'Audio-visual, photographic and information processing equipment 2');
-    $c09->addHelp('09.1.3', 'Tietojenkäsittelylaitteet', 'Audio-visual, photographic and information processing equipment 3');
-    $c09->addHelp('09.1.4', 'Tallennusvälineet', 'Audio-visual, photographic and information processing equipment 4');
-    $c09->addHelp('09.1.5', 'Audiovisuaalisten laitteiden sekä valokuvaus- ja tietojenkäsittelylaitteiden korjaus', 'Audio-visual, photographic and information processing equipment 5');
-    # $c09->addSub('09.2', 'Muut suuret vapaa-ajan välineet', 'Other major durables for recreation and culture');
-    $c09->addSub('09.2', 'Muut kulttuuriin ja vapaa-aikaan liittyvät kestokulutustavarat', 'Other major durables for recreation and culture');
-    # $c09->addSub('09.3', 'Muut virkistys- ja harrastusvälineet, puutarhanhoito sekä lemmikkieläimet', 'Other recreational items and equipment, gardens and pets');
-    $c09->addSub('09.3', 'Muut vapaa-aikaan liittyvät tarvikkeet ja laitteet, puutarhat ja lemmikkieläimet', 'Other recreational items and equipment, gardens and pets');
+    $c09->addSub('09.1', 'Audiovisuaaliset sekÃ¤ valokuvaus- ja tietojenkÃ¤sittelylaitteet', 'Audio-visual, photographic and information processing equipment');
+    $c09->addHelp('09.1.1', 'Ã„Ã¤nen ja kuvan vastaanottoon, tallentamiseen ja toistoon kÃ¤ytettÃ¤vÃ¤t laitteet', 'Audio-visual, photographic and information processing equipment 1');
+    $c09->addHelp('09.1.2', 'Valokuvaus- ja elokuvalaitteet sekÃ¤ optiset laitteet', 'Audio-visual, photographic and information processing equipment 2');
+    $c09->addHelp('09.1.3', 'TietojenkÃ¤sittelylaitteet', 'Audio-visual, photographic and information processing equipment 3');
+    $c09->addHelp('09.1.4', 'TallennusvÃ¤lineet', 'Audio-visual, photographic and information processing equipment 4');
+    $c09->addHelp('09.1.5', 'Audiovisuaalisten laitteiden sekÃ¤ valokuvaus- ja tietojenkÃ¤sittelylaitteiden korjaus', 'Audio-visual, photographic and information processing equipment 5');
+    # $c09->addSub('09.2', 'Muut suuret vapaa-ajan vÃ¤lineet', 'Other major durables for recreation and culture');
+    $c09->addSub('09.2', 'Muut kulttuuriin ja vapaa-aikaan liittyvÃ¤t kestokulutustavarat', 'Other major durables for recreation and culture');
+    # $c09->addSub('09.3', 'Muut virkistys- ja harrastusvÃ¤lineet, puutarhanhoito sekÃ¤ lemmikkielÃ¤imet', 'Other recreational items and equipment, gardens and pets');
+    $c09->addSub('09.3', 'Muut vapaa-aikaan liittyvÃ¤t tarvikkeet ja laitteet, puutarhat ja lemmikkielÃ¤imet', 'Other recreational items and equipment, gardens and pets');
     # $c09->addSub('09.4', 'Kulttuuri- ja virkistyspalvelut', 'Recreational and cultural services');
     $c09->addSub('09.4', 'Kulttuuri- ja vapaa-ajan palvelut', 'Recreational and cultural services');
     $c09->addSub('09.5', 'Sanomalehdet, kirjat ja paperitavarat', 'Newspapers, books and stationery');
@@ -128,11 +128,11 @@ class Coicop {
                        'Koulutus',
 		       'Education');
     $c10->addSub('10.1', 'Esiasteen ja alemman perusasteen koulutus', 'Pre-primary and primary education');
-    $c10->addSub('10.2', 'Ylemmän perusasteen ja keskiasteen koulutus', 'Secondary education');
-    $c10->addSub('10.3', 'Keskiasteen jälkeinen koulutus, joka ei ole korkea-asteen koulutusta', 'Post-secondary non-tertiary education');
+    $c10->addSub('10.2', 'YlemmÃ¤n perusasteen ja keskiasteen koulutus', 'Secondary education');
+    $c10->addSub('10.3', 'Keskiasteen jÃ¤lkeinen koulutus, joka ei ole korkea-asteen koulutusta', 'Post-secondary non-tertiary education');
     # $c10->addSub('10.4', 'Korkea-asteen koulutus', 'Tertiary education');
     $c10->addSub('10.4', 'Korkea-asteen koulutus ja tutkijakoulutusaste', 'Tertiary education');
-    $c10->addSub('10.5', 'Tasoltaan määrittelemätön koulutus', 'Education not definable by level');
+    $c10->addSub('10.5', 'Tasoltaan mÃ¤Ã¤rittelemÃ¤tÃ¶n koulutus', 'Education not definable by level');
     $this->cats[] = &$c10;
 
     $c11 = new CoicopCat('11', '#9900CC',
@@ -147,11 +147,11 @@ class Coicop {
     $c12 = new CoicopCat('12', '#FF00FF',
                        'Muut tavarat ja palvelut',
 		       'Miscellaneous goods and services');
-    # $c12->addSub('12.1', 'Henkilökohtainen hygienia ja kauneudenhoito', 'Personal care');
-    $c12->addSub('12.1', 'Henkilökohtainen hygienia', 'Personal care');
+    # $c12->addSub('12.1', 'HenkilÃ¶kohtainen hygienia ja kauneudenhoito', 'Personal care');
+    $c12->addSub('12.1', 'HenkilÃ¶kohtainen hygienia', 'Personal care');
     # $c12->addSub('12.2', 'Prostituutio', 'Prostitution');
-    # $c12->addSub('12.3', 'Henkilökohtaiset tavarat', 'Personal effects n.e.c.');
-    $c12->addSub('12.3', 'Henkilökohtaiset esineet, muualla luokittelemattomat', 'Personal effects n.e.c.');
+    # $c12->addSub('12.3', 'HenkilÃ¶kohtaiset tavarat', 'Personal effects n.e.c.');
+    $c12->addSub('12.3', 'HenkilÃ¶kohtaiset esineet, muualla luokittelemattomat', 'Personal effects n.e.c.');
     # $c12->addSub('12.4', 'Sosiaalipalvelut', 'Social protection');
     $c12->addSub('12.4', 'Sosiaalinen suojelu', 'Social protection');
     $c12->addSub('12.5', 'Vakuutukset', 'Insurance');
@@ -160,7 +160,7 @@ class Coicop {
     # $c12->addSub('12.7', 'Muut palvelut', 'Other services n.e.c.');
     $c12->addSub('12.7', 'Muut palvelut, muualla luokittelemattomat', 'Other services n.e.c.');
     # $c12->addSub('12.8', '---', '---');
-    $c12->addSub('12.9', 'Kulutusmenojen ulkopuoliset erät', 'Expenditure not broken down into other goods and services');
+    $c12->addSub('12.9', 'Kulutusmenojen ulkopuoliset erÃ¤t', 'Expenditure not broken down into other goods and services');
     $this->cats[] = &$c12;
 
   }
